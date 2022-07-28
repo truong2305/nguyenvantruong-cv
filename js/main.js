2993a06459a7dd3703e62b6c57e734d2778d5710
truong2305 <truongnv18dt1bkdn@gmail.com>
2022-07-28 11:27:27 +0700

    v2

--- js/main.js
+++ js/main.js
@@ -6,14 +6,43 @@ let scrollStyle = element => document.getElementById(element).scrollIntoView({be
 
 let hiddenNavbar = () =>  showNavbarClassList.remove('open-navbar')
 
+let mode = document.getElementById("mode")
+
+const getTheme = () => {
+    let theme = localStorage.getItem("theme")
+    if(theme) {
+        document.documentElement.setAttribute('data-theme', theme)
+        if(theme === "dark") {
+            mode.innerHTML = '<i class="fas fa-moon"></i>'
+        } else {
+            mode.innerHTML = '<i class="fas fa-sun"></i>'
+        }
+    }
+}
+
+mode.onclick = () => {
+    let theme = localStorage.getItem("theme")
+    if(!theme) {
+        localStorage.setItem("theme", "dark")
+    } else {
+        if(theme !== "dark") {
+            localStorage.setItem("theme", "dark")
+            mode.innerHTML = '<i class="fas fa-moon"></i>'
+        }
+        else {
+            localStorage.setItem("theme", "light")
+            mode.innerHTML = '<i class="fas fa-sun"></i>'
+        }
+    }
+    document.documentElement.setAttribute('data-theme', localStorage.getItem("theme"))
+}
+
 document.addEventListener('scroll', () => {
     if(showNavbarClassList.contains('open-navbar')) showNavbarClassList.remove('open-navbar')
-
     let btnStyle = document.getElementById('back-to-top').style
-
-    if (window.pageYOffset > 80) {
+    if (window.pageYOffset > 200) {
         btnStyle.visibility = 'visible'
-        btnStyle.opacity = 0.7
+        btnStyle.opacity = 1
     }  else {
         btnStyle.visibility = 'hidden'
         btnStyle.opacity = 0
